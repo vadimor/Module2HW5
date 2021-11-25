10480a08d53286d0c4352f36ffc2c7e2fcf58939
vadimor <vadimorbiluy@gmail.com>
2021-11-25 22:07:16 +0200

    Add exception and Helper

---
+++ Module2HW5/Helper/LogFileComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace Module2HW5.Helper
+{
+    public class LogFileComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var itemX = x as string;
+            var itemY = y as string;
+            var dateTimeX = File.GetCreationTimeUtc(itemX);
+            var dateTimeY = File.GetCreationTimeUtc(itemY);
+            return dateTimeX.CompareTo(dateTimeY);
+        }
+    }
+}
